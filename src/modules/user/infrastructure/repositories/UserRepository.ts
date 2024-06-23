bec7aa0cb4324223e4cae8420eede1327ebd6a90
Abd Al-Rahman Odeh <comodeh@gmail.com>
2024-06-23 14:22:32 +0300

    Clean up the code

--- src/modules/user/infrastructure/repositories/UserRepository.ts
+++ src/modules/user/infrastructure/repositories/UserRepository.ts
@@ -2,7 +2,6 @@ import { ApiResourceCrudRepository } from "@/modules/shared/infrastructure/repos
 import { IUserRepository } from "../../application/interfaces/IUserRepository";
 import { User } from "../../domain/User";
 
-// 沒用到
 export class UserRepository
   extends ApiResourceCrudRepository<User, string>
   implements IUserRepository {}
