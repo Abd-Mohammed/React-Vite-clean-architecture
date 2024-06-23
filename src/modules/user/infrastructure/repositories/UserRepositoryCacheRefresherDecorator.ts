c559837f63440b766555f47dc1658ad96db8f9b4
Abd Al-Rahman Odeh <comodeh@gmail.com>
2024-06-23 14:22:45 +0300

    Clean up the code

--- src/modules/user/infrastructure/repositories/UserRepositoryCacheRefresherDecorator.ts
+++ src/modules/user/infrastructure/repositories/UserRepositoryCacheRefresherDecorator.ts
@@ -1,7 +1,6 @@
 import { RepositoryCacheRefresherDecorator } from "@/modules/shared/infrastructure/repositories/RepositoryCacheRefresherDecorator";
 import { User } from "../../domain/User";
 
-// 沒用到
 export class UserRepositoryCacheRefresherDecorator extends RepositoryCacheRefresherDecorator<
   User,
   string
